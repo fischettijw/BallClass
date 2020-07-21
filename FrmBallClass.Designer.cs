18c3a614f18facbb118bb50447a4b1c0ab7f9d43
Joseph Fischetti <fischettijw@gmail.com>
2020-07-21 08:14:38 -0400

    complete

--- FrmBallClass.Designer.cs
+++ FrmBallClass.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1059, 806);
+            this.ClientSize = new System.Drawing.Size(1160, 635);
             this.Name = "FrmBallClass";
             this.Text = "Bouncing Class via Ball Class";
             this.Load += new System.EventHandler(this.FrmBallClass_Load);
--- FrmBallClass.cs
+++ FrmBallClass.cs
@@ -15,6 +15,7 @@ namespace BallClass
     {
         Timer Draw;
         BouncingBall Ball;
+        BouncingBall[] Balls = new BouncingBall[100];
         Brush BallColor;
         public FrmBallClass()
         {
@@ -29,18 +30,16 @@ namespace BallClass
 
             Ball = new BouncingBall(this, 100, 100, 5, 2, 50, BallColor);
 
+            Random rnd = new Random();
+            for (int i = 0; i < Balls.Length; i++)
+            {
+                Balls[i] = new BouncingBall(this, rnd.Next(0, 1000), rnd.Next(0, 500), rnd.Next(2, 10), rnd.Next(2, 10), rnd.Next(20, 100), new System.Drawing.SolidBrush(Color.FromArgb(rnd.Next(50, 255), rnd.Next(50, 255), rnd.Next(50, 255))));
+            }
+
             Draw = new Timer();
             Draw.Interval = 10;
             Draw.Tick += Draw_Tick;
             Draw.Enabled = true;
-        }
-
-        private void FrmBallClass_Paint(object sender, PaintEventArgs e)
-        {
-            this.BackColor = SystemColors.Control;
-            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
-            Ball.Display(e.Graphics);
-            Ball.Update();
         }
 
         private void Draw_Tick(object sender, EventArgs e)
@@ -48,6 +47,17 @@ namespace BallClass
             this.Refresh();
         }
 
+        private void FrmBallClass_Paint(object sender, PaintEventArgs e)
+        {
+            this.BackColor = SystemColors.Control;
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            foreach (BouncingBall ball in Balls)
+            {
+                ball.Display(e.Graphics);
+                ball.Update();
+            }
+        }
     }
 }
 
