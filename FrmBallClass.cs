1077f2060d7959787c5e4cb05ca6ff8386ed92fd
Joseph Fischetti <fischettijw@gmail.com>
2020-08-23 12:48:41 -0400

    first commit for xyCenter

--- FrmBallClass.cs
+++ FrmBallClass.cs
@@ -14,8 +14,8 @@ namespace BallClass
     public partial class FrmBallClass : Form
     {
         Timer Draw;
-        BouncingBall BallMaster;
-        BouncingBall[] Balls = new BouncingBall[1000];
+
+        public BouncingBall[] Balls = new BouncingBall[30];
 
         public FrmBallClass()
         {
@@ -30,14 +30,14 @@ namespace BallClass
 
             this.MouseClick += FrmBallClass_MouseClick;
 
-            //BallMaster = new BouncingBall(this, this.ClientRectangle.Width / 2, this.ClientRectangle.Height / 2, 10, 10, 250, Brushes.Black);
+
 
             BouncingBall.CreateBalls(Balls, this);
 
             this.Text = $"Bouncing Class via Ball Class  ({Balls.Length} balls)";
 
             Draw = new Timer();
-            Draw.Interval = 10;
+            Draw.Interval = 16;    // ~60 frames per second
             Draw.Tick += Draw_Tick;
             Draw.Enabled = true;
         }
@@ -59,8 +59,8 @@ namespace BallClass
 
             foreach (BouncingBall ball in Balls)
             {
-                ball.Display(e.Graphics);
-                //ball.Display(e.Graphics, OverrideClassGraphic);
+                //ball.Display(e.Graphics);
+                ball.Display(e.Graphics, OverrideClassGraphic);
                 ball.Update();
             }
 
@@ -70,10 +70,11 @@ namespace BallClass
 
         public void OverrideClassGraphic(Graphics g, BouncingBall ball)
         {
-            g.FillEllipse(ball.BallColor, ball.X, ball.Y, ball.Size, ball.Size);
-            g.FillEllipse(Brushes.Black, ball.X + (ball.Size / 8), ball.Y + (ball.Size / 8), ball.Size / 4, ball.Size / 4);
-            g.FillEllipse(Brushes.Black, ball.X + (ball.Size / 2), ball.Y + (ball.Size / 8), ball.Size / 4, ball.Size / 4);
-            g.FillEllipse(Brushes.Black, ball.X + (ball.Size / 4), ball.Y + (ball.Size / 2), ball.Size / 6, ball.Size / 6);
+            g.FillEllipse(ball.BallColor, ball.Xc, ball.Yc, ball.Size, ball.Size);
+            //g.FillEllipse(ball.BallColor, ball.X - ball.Size / 2, ball.Y - ball.Size / 2, ball.Size, ball.Size);
+            //g.FillEllipse(Brushes.Black, ball.X + (ball.Size / 8), ball.Y + (ball.Size / 8), ball.Size / 4, ball.Size / 4);
+            //g.FillEllipse(Brushes.Black, ball.X + (ball.Size / 2), ball.Y + (ball.Size / 8), ball.Size / 4, ball.Size / 4);
+            //g.FillEllipse(Brushes.Black, ball.X + (ball.Size / 4), ball.Y + (ball.Size / 2), ball.Size / 6, ball.Size / 6);
 
             //StringFormat sf = new StringFormat();
             //sf.Alignment = StringAlignment.Center;
@@ -84,6 +85,11 @@ namespace BallClass
 
             //g.DrawString($"{ball.Size}", new Font("Courier", 18), new SolidBrush(Color.Red),
             //                  ball.X + ball.Size / 2, ball.Y + ball.Size / 2);
+
+
+            g.DrawLine(new Pen(Color.Black, 2), Balls[2].Xc + Balls[2].Size / 2, Balls[2].Yc + Balls[2].Size / 2,
+                                                Balls[5].Xc + Balls[5].Size / 2, Balls[5].Yc + Balls[5].Size / 2);
+
         }
     }
 }
@@ -91,13 +97,17 @@ namespace BallClass
 
 public class BouncingBall
 {
-    int x, y, dx, dy, size;
+    private int x, y, xC, yC, dx, dy, size;
     private Brush ballColor;
     private Form frm;
 
     public int X { get { return x; } }
 
     public int Y { get { return y; } }
+
+    public int Xc { get { return xC; } }
+
+    public int Yc { get { return yC; } }
 
     public int DX { get { return x; } }
 
@@ -107,13 +117,15 @@ public class BouncingBall
 
     public Brush BallColor { get { return ballColor; } }
 
-    public BouncingBall(Form frm, int x, int y, int dx, int dy, int size, Brush ballColor)
+    public BouncingBall(Form frm, int xC, int yC, int dx, int dy, int size, Brush ballColor)
     {
-        this.x = x;
-        this.y = y;
+        this.size = size;
+        this.xC = xC;
+        this.yC = yC;
+        this.x = xC - this.size / 2;
+        this.y = yC - this.size / 2;
         this.dy = dy;
         this.dx = dx;
-        this.size = size;
         this.ballColor = ballColor;
         this.frm = frm;
     }
@@ -146,8 +158,11 @@ public class BouncingBall
 
     public void Move()
     {
-        y += dy;
-        x += dx;
+        this.xC += dx;
+        this.yC += dy;
+
+        this.x = xC - this.size / 2;
+        this.y = yC - this.size / 2;
     }
 
     public void checkCollisionsWithWalls()
@@ -180,14 +195,14 @@ public class BouncingBall
 
         for (int i = 0; i < balls.Length; i++)
         {
-            //    public BouncingBall(Form frm, int x, int y, int dx, int dy, int size, Brush ballColor)
+            //    public BouncingBall(Form frm, int xC, int yC, int dx, int dy, int size, Brush ballColor)
 
             int dir;
             balls[i] = new BouncingBall(frm,
                                         rnd.Next(0, frm.ClientRectangle.Width - 150),
                                         rnd.Next(0, frm.ClientRectangle.Height - 150),
-                                        ((dir = rnd.Next(0, 20)) - 10) == 0 ? -1 * rnd.Next(3, 20) : dir,
-                                        ((dir = rnd.Next(0, 20)) - 10) == 0 ? -1 * rnd.Next(3, 20) : dir,
+                                        ((dir = rnd.Next(0, 4)) - 10) == 0 ? -1 * rnd.Next(3, 5) : dir,
+                                        ((dir = rnd.Next(0, 4)) - 10) == 0 ? -1 * rnd.Next(3, 5) : dir,
                                         rnd.Next(35, 150),
                                         new System.Drawing.SolidBrush(Color.FromArgb(rnd.Next(50, 255),
                                                                 rnd.Next(50, 255), rnd.Next(50, 255))));
